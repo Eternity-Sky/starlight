f8c2c5a195fda227c610daf5a94767c5bb326a2c
Eternity-Sky <tengyumo666@hotmail.com>
2025-03-08 22:54:22 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,6 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'ETOJ Docs',
-			 logo: {
-        		 	src: './src/assets/49672635.png',
-      			 },
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
