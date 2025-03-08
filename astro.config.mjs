d724de2ff6c21e52b3d3f58e52c66eab8032bc32
Eternity-Sky <tengyumo666@hotmail.com>
2025-03-08 14:43:05 +0800

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,17 +12,6 @@ export default defineConfig({
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-				{
 					label: 'ET的博客',
 					autogenerate: { directory: 'Blog' },
 				},
